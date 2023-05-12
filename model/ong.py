e8a87a182ae849da76feae68b10638defc3ef963
Lucas Biz Teixeira <108308007+lucasbiz@users.noreply.github.com>
2023-05-12 17:18:02 -0300

    Update ong.py

--- model/ong.py
+++ model/ong.py
@@ -2,11 +2,6 @@ class Ong():
 
     def __init__(self, nome_ong: str):
         self.__nome_ong = nome_ong
-        self.__doacoes = []
-        self.__adocoes = []
-        self.__doadores = []
-        self.__adotantes = []
-        self.__animais = []
 
     @property
     def nome_ong(self):
@@ -17,22 +12,4 @@ class Ong():
         if isinstance(nome_ong, str):
             self.__nome_ong = nome_ong
 
-    @property
-    def doacoes(self):
-        return self.__doacoes
 
-    @property
-    def adocoes(self):
-        return self.__adocoes
-
-    @property
-    def doadores(self):
-        return self.__doadores
-
-    @property
-    def adotantes(self):
-        return self.__adotantes
-
-    @property
-    def animais(self):
-        return self.__animais
